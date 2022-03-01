db207ca6d1178936a59bd6429a4644fab47b08f9
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-02-28 18:38:07 -0600

    attained functionality

--- script-1.js
+++ script-1.js
@@ -1,129 +1,162 @@
 // all the various letters, numbers and characters that can possibly comprise a password
-var lowercase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+var lowercaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
-var uppercase = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+var uppercaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
- var numbers = [
-   '1',
-   '2',
-   '3',
-   '4',
-   '5',
-   '6',
-   '7',
-   '8',
-   '9',
-   '0'
-];
-var specialChars = [
-  '`',
-  '-',
-  '=',
-  '[',
-  ']',
-  ';',
-  ',',
-  '.',
-  '/',
-  '!',
-  '@',
-  '#',
-  '$',
-  '%',
-  '^',
-  '&',
-  '*',
-  '(',
-  ')',
-  '+'
+var numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharsArr = [
+  "`",
+  "-",
+  "=",
+  "[",
+  "]",
+  ";",
+  ",",
+  ".",
+  "/",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "+",
 ];
 // Ask user how many and what type of letter or character will constitute their password
 function getPasswordOptions() {
-  var pwdLength=parseInt(prompt("How many characters would you like your password to contain"));
+  var pwdLength = parseInt(
+    prompt("How many characters would you like your password to contain")
+  );
   if (pwdLength < 8 || pwdLength > 128) {
     alert("Password must be between 8 and 128 characters");
     return;
-  } 
-  var hasSpecialChars=confirm("Would you like special characters?");
-  var hasUppercaseLetters=confirm("Would you like uppercase letters in your password?");
-  var hasLowercaseLetters=confirm("Would you like lowercase letters in your password?");
-  var hasNumbers=confirm("Would you like numbers in your password?");
+  }
+  var hasSpecialChars = confirm("Would you like special characters?");
+  var hasUppercaseLetters = confirm(
+    "Would you like uppercase letters in your password?"
+  );
+  var hasLowercaseLetters = confirm(
+    "Would you like lowercase letters in your password?"
+  );
+  var hasNumbers = confirm("Would you like numbers in your password?");
   // series of conditionals to assign chosen letters or characters to an array
-  if (hasSpecialChars === true) {
-    var passwordArrayOne = specialChars;
-  } 
-  if (hasNumbers === true) {
-    var passwordArrayTwo = numbers;
+  if (
+    hasSpecialChars === false &&
+    hasLowercaseLetters === false &&
+    hasUppercaseLetters === false &&
+    hasNumbers === false
+  ) {
+    alert("You must select at least one option");
+    return;
   }
-  if (hasLowercaseLetters === true) {
-    var passwordArrayThree = lowercase;
-  }
-  if (hasUppercaseLetters === true) {
-    var passwordArrayFour = uppercase;
-  }
-  var finalArray = [passwordArrayOne, passwordArrayTwo, passwordArrayThree, passwordArrayFour];
-    for (var i = 0; i < pwdLength; i++) {
-    var password = finalArray[Math.floor(Math.random() * finalArray.length)];
-  }
- };
+  var optionsObj = {
+    length: pwdLength,
+    specialChars: hasSpecialChars,
+    numbers: hasNumbers,
+    uppercase: hasUppercaseLetters,
+    lowercase: hasLowercaseLetters,
+  };
+  return optionsObj;
+}
+function getRandomChar(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
 // Assignment Code
 function generatePassword() {
-  var options= getPasswordOptions()
-  console.log(options);
+  var options = getPasswordOptions();
+
+  var result = [];
+  var possibleChars = [];
+  var guaranteedChars = [];
+
+  if (options.specialChars) {
+    possibleChars = possibleChars.concat(specialCharsArr);
+    guaranteedChars.push(getRandomChar(specialCharsArr));
+  }
+  if (options.uppercase) {
+    possibleChars = possibleChars.concat(uppercaseArr);
+    guaranteedChars.push(getRandomChar(uppercaseArr));
+  }
+  if (options.lowercase) {
+    possibleChars = possibleChars.concat(lowercaseArr);
+    guaranteedChars.push(getRandomChar(lowercaseArr));
+  }
+  if (options.numbers) {
+    possibleChars = possibleChars.concat(numbersArr);
+    guaranteedChars.push(getRandomChar(numbersArr));
+  }
+  for (var i = 0; i < options.length; i++) {
+    var possibleChar = getRandomChar(possibleChars);
+    result.push(possibleChar);
+    console.log(possibleChar);
+  }
+  for (var i = 0; i < guaranteedChars.length; i++) {
+    result[i] = guaranteedChars[i];
+  }
+  return result.join("");
 }
+//guaranteedChars=result
+//method called join which will combine arrays like result.join
 
 var generateBtn = document.querySelector("#generate");
 
@@ -133,7 +166,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
